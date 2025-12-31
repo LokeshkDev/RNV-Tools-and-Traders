8e4f13f2a6de1426ef1581737705c26c01ce1bb1
lokeshk2492 <lokeshk2492@gmail.com>
2025-12-31 15:53:24 +0530

    gallery loading issue fix

--- js/main.js
+++ js/main.js
@@ -43,15 +43,17 @@
 
 
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+    $(window).on('load', function () {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+        $('#portfolio-flters li').on('click', function () {
+            $("#portfolio-flters li").removeClass('active');
+            $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+            portfolioIsotope.isotope({ filter: $(this).data('filter') });
+        });
     });
 
 
